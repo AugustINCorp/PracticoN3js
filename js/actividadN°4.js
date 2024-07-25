cf1f7dc73c9579b3d5faffc9121d6719afb4e23e
Veroncho <augustoveron41@gmail.com>
2024-07-25 13:48:16 -0300

    se agregaron cambios y algunos ejercicios

--- js/actividadN°4.js
+++ js/actividadN°4.js
@@ -1,13 +1,12 @@
 /*
 4- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
-*/const numero = parseInt(prompt("Ingresa un número:"));
+*/
+console.log(numero)
 
-function parOImpar(numero) {
+function numParImpar(numero = 4) {
   if (numero % 2 === 0) {
-    return document.write("<p>Tu número es par</p>");
+    console.log(`<p>Tu número es Par: ${numero}</p>`)
   } else {
-    return document.write("<p>Tu número es impar</p>");
+    console.log(`<p>Tu número es Impar: ${numero}</p>`)
   }
 }
-
-console.log(parOImpar)
